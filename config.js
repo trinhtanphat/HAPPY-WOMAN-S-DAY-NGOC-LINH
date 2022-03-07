52c2d4c861e6972c00406e09cbc3df49a64297de
trinhtanphat <89531707+trinhtanphat@users.noreply.github.com>
2022-03-07 21:30:56 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Gửi bạn Ngọc Linh <3!';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100069836841051';
+const eventName = "HAPPY WOMAN'S DAY!";
+const titleCard = 'Gửi bạn Linh';
+const contentCard = 'Wishing you a whole lot of happiness, today and for the rest of the year. Every home, every heart, every feeling, every moment of happiness is incomplete without you, only you can complete this world. Happy Women’s Day!';
+
+const giftImage = 'a1.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
